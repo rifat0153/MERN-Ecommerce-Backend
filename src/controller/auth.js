9e383c0cfdeeb9d8e972723d63bf46da6a1effd1
Mahbubur Rahman <rifat0153@gmail.com>
2021-02-03 11:27:48 +0600

    Validators Added to Signup and Signin

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,7 +1,14 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 
 exports.signup = (req, res) => {
+
+    
+
+
+
+
     User.findOne({  email: req.body.email })
     .exec((error, user) => {
         if(user) return res.status(400).json({
@@ -71,5 +78,6 @@ exports.requireSignin = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
-    next()
+    
+    next();
 }
